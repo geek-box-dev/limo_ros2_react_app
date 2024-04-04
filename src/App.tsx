57b5899313141231c0b5a62c7b67bdbd820016e9
akihisa-sumi <akihisa.sumi@cozmic.io>
2024-04-04 14:21:08 +0900

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,68 @@
+import React, { useCallback, useEffect, useState } from "react";
+import ROSLIB, { Ros } from "roslib";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  let ros: any = undefined;
+
+  useEffect(() => {
+    const _ros = new ROSLIB.Ros({
+      url: "ws://192.168.1.8:9090",
+    });
+    console.log("API called");
+
+    _ros.on("connection", () => {
+      ros = _ros;
+      console.log("Connected to ROSBridge WebSocket server.");
+    });
+
+    _ros.on("error", function (error: any) {
+      console.log("Error connecting to ROSBridge WebSocket server: ", error);
+    });
+
+    _ros.on("close", function () {
+      console.log("Connection to ROSBridge WebSocket server closed.");
+    });
+  }, []);
+
+  const publishCmd = useCallback(() => {
+    console.log(ros);
+    if (!ros?.isConnected) {
+      console.log("ros is undefined");
+      return;
+    }
+    const cmdVel = new ROSLIB.Topic({
+      ros,
+      name: "/cmd_vel",
+      messageType: "geometry_msgs/Twist",
+    });
+    const twist = new ROSLIB.Message({
+      linear: {
+        x: 2.0,
+        y: 0,
+        z: 0,
+      },
+      angular: {
+        x: 0,
+        y: 0,
+        z: 0,
+      },
+    });
+    cmdVel.publish(twist);
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <button onClick={publishCmd}>Publish Cmd</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
